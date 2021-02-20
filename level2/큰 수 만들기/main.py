62eaecd5d29db3101e070e7c05cbd0607a410f5d
Jionee <49470328+Jionee@users.noreply.github.com>
2021-02-20 16:01:48 +0900

    Update main.py

--- level2/큰 수 만들기/main.py
+++ level2/큰 수 만들기/main.py
@@ -1,3 +1,35 @@
+from collections import deque
+def solution(number, k):
+    answer = ''
+    
+    numberList = deque(list(map(int,number)))
+    require = len(number)-k
+    answerList = []
+    while len(numberList) != require or require ==0 : #list 길이와 남은 숫자 자릿수가 같을 때까지
+        
+        maxNum = numberList[0]
+        for i in range(len(numberList)-require) :
+            maxNum = numberList[i+1] if maxNum < numberList[i+1] else maxNum
+            if maxNum == 9:
+                break
+        maxIndex = numberList.index(maxNum) #최대 인덱스
+    
+        for j in range(0,maxIndex+1): #list 재구성
+            numberList.popleft()
+        
+        answerList.append(maxNum)
+        require-=1
+        
+        if require == 0:
+            break
+        elif len(numberList) == require: 
+            while len(numberList)!=0:
+                answerList.append(numberList.popleft())
+            break
+    
+    answer = "".join(map(str,answerList))
+    return answer
+
 #testcase 9,10 시간초과
 
 def solution(number, k):
