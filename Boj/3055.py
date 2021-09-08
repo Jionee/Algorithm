8e2ac4abefb8bedfa73f5a3aae37ac39da8134a7
jiwon_ryu <happyjw3910@gmail.com>
2021-09-08 18:50:39 +0900

    3055

--- Boj/3055.py
+++ Boj/3055.py
@@ -1,90 +1,64 @@
 import sys
+from collections import deque
 
-# 3 3
-# D.*
-# ...
-# .S.
+def input():
+    return sys.stdin.readline().rstrip()
 
-input = sys.stdin.readline
+R, C = map(int, input().split())
 
-row, column = map(int, sys.stdin.readline().split(" "))
-qMap = [list(sys.stdin.readline().strip()) for _ in range(row)]
-print(qMap)
+board = []
+visited = [[False for _ in range(C)] for _ in range(R)]
+#물, 고슴도치를 큐에 넣고 움직이면서 굴에 들어갔는지 확인할거야
+#visited 즉 고슴도치가 움직인 곳은 볼 필요 없겠지?
 
-# 상하좌우 (행,열 순)
-arrow = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-answer = 0
+for _ in range(R):
+    board.append(list(input()))
 
+cnt = 0
+success = False #굴을 발견했는지
+q = deque([])
+direction = [(-1,0),(1,0),(0,1),(0,-1)]
 
-# 한 턴 지날 때마다 answer ++
-# 물 확장 가능한지 확인 -> 다음 비버 이동 가능한 경우 모두 재귀 돌리기
-# 물은 돌X,DX / S만나면 return
-# 비버는 돌X,물X
-# 끝나는 조건은 비버가 D만났을 때 / 비버 죽었을 때 (return)
+#큐에 물, 고슴도치 순으로 넣는다.
+for r in range(R):
+    for c in range(C):
+        if board[r][c] == "*": #물이면
+            q.append(("*",0,r,c))
 
-def widenWater(qMap):
-    for r in range(row):
-        for c in range(column):
-            if qMap[r][c] == '*':
-                # 물 상하좌우 비교
-                for a in arrow:
-                    ro = r + a[0]
-                    co = c + a[1]
-                    # 벽
-                    if ro < 0 or ro > r - 1 or co < 0 or co > c - 1:
-                        continue
-                    # 돌(X),굴(D),물(*)
-                    elif qMap[ro][co] == 'X' or qMap[ro][co] == 'D' or qMap[ro][co] == '*':
-                        continue
-                    # 고슴도치 만남
-                    elif qMap[ro][co] == 'S':
-                        return "DEATH"
-                    else:
-                        qMap[ro][co] = '*'
-    return qMap
+for r in range(R):
+    for c in range(C):
+        if board[r][c] == "S": #고슴도치면
+            q.append(("S",0,r,c))
+            visited[r][c] = True
 
-
-def moveBeaver(qMap, pAnswer):
-    for r in range(row):
-        for c in range(column):
-            if qMap[r][c] == 'S':
-                # 비버 상하좌우 비교
-                for a in arrow:
-                    ro = r + a[0]
-                    co = c + a[1]
-                    # 벽
-                    if ro < 0 or ro > r - 1 or co < 0 or co > c - 1:
-                        continue
-                    # 돌(X),물(*)
-                    elif qMap[ro][co] == 'X' or qMap[ro][co] == '*':
-                        continue
-                    # 굴(D)
-                    elif qMap[ro][co] == 'D':
-                        return 'SUCCESS'
-                    # 빈칸 - 이동 가능
-                    elif qMap[ro][co] == '.':
-                        qMap[r][c] = '.'
-                        qMap[ro][co] = 'S'
-                        # 재귀함수
-                        pAnswer += 1
-                        doGame(qMap, pAnswer)
-                    # 더 이상 이동 불가
-                    else:
-                        return 'KAKTUS'
+while q: # 큐가 빌 때까지 진행
+    what,cnt,now_r,now_c = q.popleft()
+    for _r, _c in direction:
+        nxt_r, nxt_c = now_r + _r, now_c + _c
+        if not (0 <= nxt_r < R) or not (0 <= nxt_c <C): #벽이면
+            continue
+        if what == "*": #물
+            if board[nxt_r][nxt_c] == ".":
+                board[nxt_r][nxt_c] = "*"
+                visited[nxt_r][nxt_c] = True
+                q.append((what,cnt+1,nxt_r,nxt_c))
+        else: #고슴도치
+            if visited[nxt_r][nxt_c]: #이미 방문했으면
+                continue
+            if board[nxt_r][nxt_c] == ".": #이동가능
+                q.append((what,cnt+1,nxt_r,nxt_c))
+                visited[nxt_r][nxt_c] = True
+            elif board[nxt_r][nxt_c] == "D": #굴 도착
+                success = True
+                break
+    if success:
+        break
+if success:
+    print(cnt+1)
+else:
+    print("KAKTUS")
 
 
-def doGame(Map, pAnswer):
-    # 물 확장
-    wWater = widenWater(Map)
-    # answer 줄이기
-    if wWater == "DEATH":
-        return "DEATH"
-    mBeav = moveBeaver(wWater, pAnswer)
-    if mBeav == "SUCCESS":
-        print(pAnswer)
-        quit()
 
 
-# main함수
-answer = 0
-doGame(qMap, answer)
+
