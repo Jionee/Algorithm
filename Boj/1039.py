7f5feb4fb89c2d55b3eb545b2d30e24ea2fe4309
jiwon_ryu <happyjw3910@gmail.com>
2021-10-06 19:21:12 +0900

    1039 dfs벗 시간초과

--- Boj/1039.py
+++ Boj/1039.py
@@ -1,40 +1,46 @@
 import sys
+sys.setrecursionlimit(10**5)
+
 N,K = sys.stdin.readline().rstrip().split(" ")
 #print(N,K)
 K = int(K)
-#K번 교환을 했을 때 만들 수 있는 가장 큰 수
-#0으로 시작하면 안됨 -> 1번째 자리수가 0이면 안됨
+M = len(N)
+#자연수 N을 K번 교환 했을 때 만들 수 있는 가장 큰 수
+#0으로 시작하면 안됨 -> 1번째 자리수가 0이면 안됨 (바꿀 때 i,j번째를 바꾼다고 하면 i==0 && num[j]==0이면 못바꿈(넘어가기)
 
-#연산을 K번할 수 없으면 -1 출력 -> 어떤 경우?(-> 두자리인데 0이 포함되어 있고 K가 홀수일때)
-if (len(N) == 2 and N[1] == '0') or (len(N)==1 and K > 1):
+#연산을 K번할 수 없으면 -1 출력 -> 어떤 경우? (-> 두자리인데 두번째 자리수가 0, 한 자리수)
+#N<=1,000,000이므로 dfs라는 것을 생각해볼 수 있음. 다 해보기
+
+if (len(N)==2 and N[1]=='0') or len(N)==1:
     print(-1)
-    exit()
+    quit()
 
-num = list(N)
-#print(num)
+def swap(tmp,a,b):
+    tmp[a], tmp[b] = tmp[b], tmp[a]
+    return tmp
 
-#리스트에서 최대 수를 찾아서 i번째랑 그 max번째랑 swap #i는 0번째부터 바꾸기
-#근데 만약 i랑 max랑 같으면 k증가 안하고 넘어감
-#k번 다하면 종료
-k = 0
+answer = 0
+#visited는 필요없음(계속 방문가능하니까)
+def dfs(num,k):
+    global answer
+    #종료조건
+    if k >= K:
+        answer = max(answer,int(''.join(num)))
+        return answer
+    #if(조건) dfs시행
+    for a in range(M):
+        for b in range(M):
+            #print(a, b)
+            if a!=b:
+                #print(a,b)
+                if a == 0 and num[b] == '0': #0이 맨 앞에 올 수 없음
+                    continue
+                # tmp = num
+                # tmp[a],tmp[b] = tmp[b],tmp[a] #자리 바꾸기
+                dfs(swap(num,a,b),k+1)
+                swap(num, a, b) #되돌리기
+                #print("(a,b):",a,b, "swap 후 num:",swap(num,a,b),"증가후k:",k,"num은?",num)
+    return
 
-for i in range(K*2):
-    if k >= K: #k번 다 함
-        break
-    criteria = num[i:]
-    #print("****",criteria)
-    if len(criteria) <= 1:
-        #swap
-        num[len(num)-1],num[len(num)-2] = num[len(num)-2],num[len(num)-1]
-        #print("criteria<=1",num,k)
-    else:
-        maxIndex = i + criteria.index(max(criteria))
-        if i == maxIndex:
-            #print("i==maxindex", i,maxIndex,num,k)
-            continue
-        #swap, k 증가
-        num[i],num[maxIndex] = num[maxIndex],num[i]
-        #print("swap하기",num,"k = ",k,"maxIndex = ",maxIndex,"i = ",i)
-    k += 1
-
-print(''.join(num))
+dfs(list(N),0)
+print(answer)
