697db03b275f5f1165880f13c70c4b7b055eef3d
jiwon_ryu <happyjw3910@gmail.com>
2021-11-15 11:40:38 +0900

    15686

---
+++ Boj/15686.py
@@ -0,0 +1,29 @@
+import sys
+from itertools import combinations
+
+def input():
+    return sys.stdin.readline().rstrip()
+
+N,M = map(int,input().split(" "))
+maps = [list(map(int,input().split(" "))) for _ in range(N)]
+house = [(a+1,b+1) for a in range(N) for b in range(N) if maps[a][b] == 1]
+chicken = [(a+1,b+1) for a in range(N) for b in range(N) if maps[a][b] == 2]
+
+#치킨거리 계산
+def calculate(house,chicken):
+    return abs(house[0] - chicken[0]) + abs(house[1] - chicken[1])
+
+#모든 경우의 수 다 해서 치킨거리의 합이 가장 작은걸 채택
+cand_chicken = list(combinations(chicken,M))
+#print("치킨집후보 : ",cand_chicken)
+
+sum_min = float('inf')
+for cand in cand_chicken: #치킨집 후보들을 골라서 치킨거리 최소 구하기
+    sum = 0
+    for h in house: #각 집에서 치킨집까지의 치킨거리 구하기
+        minChicken = float('inf')
+        for c in cand:
+            minChicken = min(calculate(h,c),minChicken) #계산된 거리 중 최소 값
+        sum += minChicken #치킨거리 계산
+    sum_min = min(sum,sum_min) #후보 중 치킨거리 최솟값
+print(sum_min)
