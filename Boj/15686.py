ec94083c45abf2825b9c363eb04208599ff8cf2e
jiwon_ryu <happyjw3910@gmail.com>
2021-11-16 14:01:18 +0900

    15686

--- Boj/15686.py
+++ Boj/15686.py
@@ -14,16 +14,10 @@ def calculate(house,chicken):
     return abs(house[0] - chicken[0]) + abs(house[1] - chicken[1])
 
 #모든 경우의 수 다 해서 치킨거리의 합이 가장 작은걸 채택
-cand_chicken = list(combinations(chicken,M))
-#print("치킨집후보 : ",cand_chicken)
-
 sum_min = float('inf')
-for cand in cand_chicken: #치킨집 후보들을 골라서 치킨거리 최소 구하기
+for cand in combinations(chicken,M): #치킨집 후보들을 골라서 치킨거리 최소 구하기
     sum = 0
     for h in house: #각 집에서 치킨집까지의 치킨거리 구하기
-        minChicken = float('inf')
-        for c in cand:
-            minChicken = min(calculate(h,c),minChicken) #계산된 거리 중 최소 값
-        sum += minChicken #치킨거리 계산
+        sum += min(calculate(h,c) for c in cand)
     sum_min = min(sum,sum_min) #후보 중 치킨거리 최솟값
 print(sum_min)
