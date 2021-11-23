e6e7f6bb145c1a9255be3c7d2291309057d5d311
jiwon_ryu <happyjw3910@gmail.com>
2021-11-23 22:36:33 +0900

    1806 시간초과

--- Boj/1806.py
+++ Boj/1806.py
@@ -1,10 +1,36 @@
 import sys
-from itertools import combinations
+def input():
+    return sys.stdin.readline().rstrip()
 
-n,m = map(int,sys.stdin.readline().split(" "))
-num = list(map(int,sys.stdin.readline().split(" ")))
+N,S = map(int,input().split(" "))
+su = list(map(int,input().split(" ")))
+#print(su)
 
-print(num)
-for i in range(len(num)):
-    comb = list(combinations(num,i))
-    print(comb)
+answer = float('inf')
+#연속된 수들의 부분합 중에 합이 S이상이 되는 것 중, 가장 짧은 길이
+#일단 0부터 각 i까지의 합을 구해놓기
+dp = [0 for _ in range(N)]
+dp[0] = su[0]
+
+for i in range(1,N):
+    dp[i] = dp[i-1] + su[i]
+    if dp[i] >= S:
+        answer = i + 1
+#print(dp)
+
+#합 생성 불가
+if dp[-1] < S:
+    print(0)
+    exit()
+
+#각 자리부터 돌면서 연속된 거 중에 S보다 큰거 찾기
+for i in range(1,N):
+    for k in range(i,N):
+        target = dp[k] - dp[i-1]
+        if target >= S:
+            answer = min(answer,k-i+1)
+            break
+    #0이면 그냥 dp
+    #1이면 dp[i]-dp[i-1]
+
+print(answer)
