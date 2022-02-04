0217cf4bd309c7e1b343557b53c908967231ffd9
jiwon_ryu <happyjw3910@gmail.com>
2022-02-04 16:51:58 +0900

    1837 소수

--- AlgorithmStudy/src/main/java/Boj1837_암호제작.java
+++ AlgorithmStudy/src/main/java/Boj1837_암호제작.java
@@ -1,85 +1,47 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Boj1837_암호제작 {
-    static int MAX = 1000000;
-    static String P;
+    static BigInteger P;
     static int K;
+    static boolean isPrime[];
+    static ArrayList<Integer> primeList = new ArrayList<>();
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("src/main/java/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        P = st.nextToken();
+        P = new BigInteger(st.nextToken());
         K = Integer.parseInt(st.nextToken());
+
+        isPrime = new boolean[K+1]; //2~K까지
+        for(int i=2;i<K;i++){
+            if(isPrime[i]){ //소수면 넘어감
+                continue;
+            }
+            if((P.remainder(BigInteger.valueOf(i))).compareTo(BigInteger.valueOf(0))==0){
+                System.out.println("BAD "+ i);
+                return;
+            }
+            //배수 삭제
+            for(int j = i+i;j<K;j+=i){
+                isPrime[j] = true;
+            }
+        }
+
+        System.out.println("GOOD");
 
         //1. K보다 작은 수 중 소수를 찾는다.
         //2. 하나라도 P % 해당 소수 == 0 -> BAD
         //3. 다 통과하면 -> GOOD
 
-        //1. k보다 작은 수 중 소수 찾기
-        ArrayList<Integer> Primes = new ArrayList<>();
-        boolean[] isNotPrime = new boolean[MAX+1];
-        for(int i = 2; i < MAX + 1; i++){
-            if(isNotPrime[i] == false){
-                Primes.add(i);
-                //System.out.println(Arrays.toString(isNotPrime));
-                for(int j = i *2; j< MAX + 1;j+=i){
-                    isNotPrime[j] = true;
-                }
-            }
-        }
+        //1. k보다 작은 수 중 소수 찾기 (에라토스테네스의 체 이용)
+        //2~K중 false인 것 중에 최소인것
 
-        for (Integer prime : Primes) {
-            if(prime >= K){
-                break;
-            }
-            //System.out.println(prime);
-            //P % 소수 == 0 -> BAD, 소수 -> break;
-            //앞에서부터 한자리씩 가져와서 %연산 후 마지막 값이 0인지 확인
-            String A = "";
-            int mod = 0;
-            for(int i=0;i<P.length();i++){
-                A += Character.toString(P.charAt(i));
-                mod = Integer.parseInt(A)% prime;
-                if(i>0){
-                    A = Character.toString(A.charAt(1));
-                }
-                System.out.println(A+"/"+prime+"="+mod);
-            }
-            if(mod == 0){
-                System.out.println("BAD "+prime);
-                return;
-            }
-        }
-
-//        for(int prime:Primes){
-//            if (prime >= K){
-//                break;
-//            }
-//            if(checkIsBad(prime)){
-//                System.out.println("BAD" + prime);
-//                return;
-//            }
-//        }
-        //이외의 경우는 GOOD
-        System.out.println("GOOD");
-    }
-    static boolean checkIsBad(int x){
-        int r = 0;
-        for (int i=0;i<P.length();i++){
-            r = (r*10 +(P.charAt(i) - '0')) % x;
-        }
-        if(r==0){
-            return true;
-        }
-        else{
-            return false;
-        }
     }
 }
