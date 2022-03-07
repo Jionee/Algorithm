ef9816b148a899386785d870edbcebd7c3a33f94
jiwon_ryu <happyjw3910@gmail.com>
2022-03-07 12:24:52 +0900

    16197 bfs 조건까다로움

--- AlgorithmStudy/src/main/java/Boj16197_두동전.java
+++ AlgorithmStudy/src/main/java/Boj16197_두동전.java
@@ -11,8 +11,7 @@ public class Boj16197_두동전 {
     static int[] dRow = {-1,1,0,0}; //상하좌우
     static int[] dCol = {0,0,-1,1};
     static Point start;
-    static boolean[][] visit1;
-    static boolean[][] visit2;
+    static boolean[][][][] visit;
 
     public static void main(String[] args) throws Exception{
         System.setIn(new FileInputStream("src/main/java/input.txt"));
@@ -22,8 +21,7 @@ public class Boj16197_두동전 {
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         Map = new char[N+1+1][M+1+1];
-        visit1 = new boolean[N+1+1][M+1+1];
-        visit2 = new boolean[N+1+1][M+1+1];
+        visit = new boolean[N+1+1][M+1+1][N+1+1][M+1+1];
 
         for(int i=1;i<N+1;i++){
             String str = br.readLine();
@@ -42,22 +40,14 @@ public class Boj16197_두동전 {
         }
 
         //상하좌우(4가지 경우의수)
-        //두 동전 중 하나만 떨어뜨렸을 때 버튼의 최소 횟수 (최소 횟수면 bfs고고)
+        //두 동전 중 하나만 떨어뜨렸을 때 버튼의 최소 횟수 (최소 횟수면 bfs고고) //bfs는 너비우선탐색이기 때문에 visit을 다시 되돌려놓을 필요가 없다.
         //두 동전을 떨어뜨릴 수 없거나, 버튼을 10번보다 많이 눌러야 하면 -1 출력
         Queue<Point> queue = new LinkedList<>();
         queue.add(start);
-        visit1[start.row1][start.col1] = true;
-        visit2[start.row2][start.col2] = true;
+        visit[start.row1][start.col1][start.row2][start.col2] = true;
 
         while(!queue.isEmpty()){
             Point now = queue.poll();
-
-
-            System.out.println(now);
-            for (Point point : queue) {
-                System.out.println("QUEUE - "+point);
-            }
-
 
             //1.목적지인가? -> 버튼 10이상(count>=10), 하나만 떨어짐(함수로빼서 체크,둘 중 하나만 true면 끝)
             if(now.count>=10){
@@ -73,55 +63,54 @@ public class Boj16197_두동전 {
                 int newRow2 = now.row2 + dRow[i];
                 int newCol2 = now.col2 + dCol[i];
 
-//                if(!isEnd(newRow1,newCol1) && Map[newRow1][newCol1]=='#'){ //벽이면 이동안함
-//                    newRow1 = now.row1;
-//                    newCol1 = now.col1;
-//                }
-//                if(!isEnd(newRow2,newCol2) && Map[newRow2][newCol2]=='#'){
-//                    newRow2 = now.row2;
-//                    newCol2 = now.col2;
-//                }
-
-                if(!canMoveCoin(newRow1,newCol1)){ //벽이면 이동안함
+                if(isWall(newRow1,newCol1)){ //벽이면 이동안함(그대로)
                     newRow1 = now.row1;
                     newCol1 = now.col1;
                 }
-                if(!canMoveCoin(newRow2,newCol2)){
+                if(isWall(newRow2,newCol2)){
                     newRow2 = now.row2;
                     newCol2 = now.col2;
                 }
 
-                //System.out.println(newRow1+","+newCol1+","+newRow2+","+newCol2);
-                boolean end1 = isEnd(now.row1,now.col1);
-                boolean end2 = isEnd(now.row2,now.col2);
-                if(end1 ^ end2){
-                    System.out.println(now.count);
+                boolean end1 = isEnd(newRow1,newCol1); //떨어졌는지 - (1)(2)(3)
+                boolean end2 = isEnd(newRow2,newCol2);
+                if(end1 ^ end2){ //(1)하나만 떨어지는 경우
+                    System.out.println(now.count+1);
                     return;
                 }
-                else if(!visit1[newRow1][newCol1] && !visit2[newRow2][newCol2] && !isEnd(newRow1,newCol1) && !isEnd(newRow2,newCol2)){
-                    //4. 간다
-                    visit1[now.row1][now.col1] = true;
-                    visit2[now.row2][now.col2] = true;
-                    queue.add(new Point(newRow1,newCol1,newRow2,newCol2,now.count+1));
+                else if(end1 && end2){ //(2)두개 다 떨어지는 경우
+                    continue;
                 }
+
+                //(3)두개 다 안떨어지는 경우
+                if(!(0<newRow1 && newRow1<=N && 0<newCol1 && newCol1<=M && 0<newRow2 && newRow2<=N && 0<newCol2 && newCol2<=M)){ //벽
+                    continue;
+                }
+                if(visit[newRow1][newCol1][newRow2][newCol2]){ //방문체크
+                    continue;
+                }
+
+                //4.간다
+                visit[newRow1][newCol1][newRow2][newCol2] = true; //0. 체크인
+                queue.add(new Point(newRow1,newCol1,newRow2,newCol2,now.count+1));
             }
         }
         //못떨어뜨리면 -1
-        System.out.println("ASDF"+"-1");
+        System.out.println(-1);
     }
-    public static boolean canMoveCoin(int nx, int ny) {
-        if(nx >= 0 && ny >= 0 && nx < N && ny < M && Map[nx][ny] == '#') {
-            return false;
+
+    public static boolean isWall(int nx, int ny) {
+        if(nx > 0 && ny > 0 && nx <= N && ny <= M && Map[nx][ny] == '#') { //벽
+            return true;
         }
-        return true; //떨어지는게 true
+        return false;
     }
 
     static boolean isEnd(int row,int col){
-        //벽이면 true, 아니면 false 리턴
         if(0<row && row<=N && 0<col && col<=M){
             return false;
         }
-        return true;
+        return true; //게임끝
     }
 
     static class Point{
@@ -130,17 +119,6 @@ public class Boj16197_두동전 {
         int row2;
         int col2;
         int count;
-
-        @Override
-        public String toString() {
-            return "Point{" +
-                    "row1=" + row1 +
-                    ", col1=" + col1 +
-                    ", row2=" + row2 +
-                    ", col2=" + col2 +
-                    ", count=" + count +
-                    '}';
-        }
 
         public Point(int row1, int col1) {
             this.row1 = row1;
