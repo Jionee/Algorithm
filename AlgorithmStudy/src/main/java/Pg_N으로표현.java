f922cfc33a94435321fb52a48efc34fd9b10112a
jiwon_ryu <happyjw3910@gmail.com>
2022-03-28 21:27:18 +0900

    N으로표현 dp 짱어려워서 이해못함 담에 다시..

--- AlgorithmStudy/src/main/java/Pg_N으로표현.java
+++ AlgorithmStudy/src/main/java/Pg_N으로표현.java
@@ -1,13 +1,35 @@
 import java.io.*;
 import java.util.StringTokenizer;
+import java.util.TreeSet;
 
-public class Pg_Basic {
+//2:51~
+public class Pg_N으로표현 {
     static StringTokenizer st;
+    static int N = 5;
+    static int number = 12;
+    static int answer;
 
     public static void main(String[] args) throws Exception{
-        System.setIn(new FileInputStream("src/main/java/input.txt"));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        answer = Integer.MAX_VALUE;
 
+        dfs(0,0);
+
+        System.out.println(answer);
+        //return answer==Integer.MAX_VALUE?-1:answer;
+    }
+    public static void dfs(int count, int sum){
+        if(count>8) return;
+        if(number==sum){
+            answer = Math.min(answer,count);
+        }
+        System.out.println("count = "+count+", sum = "+sum);
+        int X = N;
+        for(int i=1;i<=8-count;i++){
+            dfs(i+count,sum+X);
+            dfs(i+count,sum-X);
+            dfs(i+count,sum*X);
+            dfs(i+count,sum/X);
+            X = (10*X)+N;
+        }
     }
 }
