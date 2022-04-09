64460b501c3eaec6f4bcd08bf65d6b6bf6aaeb8c
jiwon_ryu <happyjw3910@gmail.com>
2022-04-09 22:09:59 +0900

    124나라의 숫자

--- AlgorithmStudy/src/main/java/Pg_124나라의숫자.java
+++ AlgorithmStudy/src/main/java/Pg_124나라의숫자.java
@@ -1,20 +1,40 @@
-import java.util.*;
+import java.util.HashMap;
 
-public class Pg_Basic {
+public class Pg_124나라의숫자 {
 
     public static void main(String[] args) throws Exception{
         //input
-
-        Solution.solution();
+        int n = 5;
+        Solution.solution(n);
     }
 
     static class Solution {
-        /**/
-        public static int[] solution() {
-            int[] answer = {};
-            System.out.println("asdf");
+        static char[] newNumber = {'1','2','4'};
+        static String answer;
+        public static String solution(int n) {
+            answer = "";
+
+            StringBuilder sb = new StringBuilder();
+            HashMap<Integer,Character> Map = new HashMap<>();
+            Map.put(1,'1');
+            Map.put(2,'2');
+            Map.put(0,'4');
+            int mock = n;
+            int namuji = 0;
+            while(true){
+                mock = n / 3;
+                namuji = n % 3;
+                n = mock;
+                sb.append(Map.get(namuji));
+                if(mock==0){
+                    break;
+                }
+            }
+            answer = sb.reverse().toString();
+
+            System.out.println(answer);
             return answer;
         }
-        /**/
+
     }
 }
