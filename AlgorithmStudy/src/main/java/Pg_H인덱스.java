104a01a221ad2bd8e1e8161db103375b245b0abf
jiwon_ryu <happyjw3910@gmail.com>
2022-04-15 23:31:03 +0900

    H인덱스

--- AlgorithmStudy/src/main/java/Pg_H인덱스.java
+++ AlgorithmStudy/src/main/java/Pg_H인덱스.java
@@ -1,13 +1,42 @@
 import java.io.*;
 import java.util.StringTokenizer;
 
-public class Pg_Basic {
+public class Pg_H인덱스 {
     static StringTokenizer st;
+    static int N;
+    static int[] citations;
 
-    public static void main(String[] args) throws Exception{
+    public static int main(String[] args) throws Exception{
         System.setIn(new FileInputStream("src/main/java/input.txt"));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int answer = 0;
+        N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            citations[i] = Integer.parseInt(st.nextToken());
+        }
 
+        //논문 h회이상 인용이 h번 이상 -> h의 최댓값 -> 이분탐색 쌉가능?
+        int start = 0;
+        int end = 1000;
+        while(start<=end){
+            int mid = (start+end)/2;
+            int count = 0;
+            for(int i=0;i<citations.length;i++){
+                if(citations[i]>=mid){
+                    count++;
+                }
+            }
+            if(count>=mid){
+                start = mid + 1;
+                answer = Math.max(answer,mid);
+            }
+            else{
+                end = mid - 1;
+            }
+        }
+
+        return answer;
     }
 }
