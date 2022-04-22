ad4c0a51887a5e83977638da4fea9be7884d108c
Jionee <dnjswldb98@naver.com>
2022-04-22 13:33:21 +0900

    Window Test

---
+++ AlgorithmStudy/src/main/java/Boj17144_미세먼지안녕.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Boj17144_미세먼지안녕 {
+    static StringTokenizer st;
+    static int N;
+    static int[] Arr;
+    static int Answer;
+
+    public static void main(String[] args) throws Exception{
+        //System.setIn(new FileInputStream("src/main/java/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+       // N = Integer.parseInt(br.readLine());
+        System.out.println("Window 연결 TEST");
+    }
+
+}
