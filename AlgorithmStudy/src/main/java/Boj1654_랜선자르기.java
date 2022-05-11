4ddb74e1ccfa9778b6b75ab30108ed0f8af446ee
jiwon_ryu <happyjw3910@gmail.com>
2022-05-11 11:01:19 +0900

    2629 dp

---
+++ AlgorithmStudy/src/main/java/Boj1654_랜선자르기.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Boj10815_숫자카드 {
+    static StringTokenizer st;
+    static int N;
+
+    public static void main(String[] args) throws Exception{
+        System.setIn(new FileInputStream("src/main/java/input.txt"));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        N = Integer.parseInt(br.readLine());
+
+    }
+}
